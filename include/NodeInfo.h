6069d15b290b74e496a3104138e8e51b65af63a9
Xinkai Jiang <jiangxinkai98@gmail.com>
2025-01-16 09:27:51 +0100

    first commit

---
+++ include/NodeInfo.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#include <vector>
+
+struct NodeAddress {
+    std::string ip;
+    int port;
+};
+
+struct NodeInfo {
+    std::string name;
+    std::string nodeID;
+    NodeAddress addr;
+    std::string type;
+    int servicePort;
+    int topicPort;
+    std::vector<std::string> serviceList;
+    std::vector<std::string> topicList;
+};
+
